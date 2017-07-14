26d3e1e64ad9bbf520b6fdb06eefd3bf749fb3e4
buluceli <xwlyj@126.com>
2017-07-14 15:54:56 +0800

    share  wechat

--- src/main/java/com/reke/learn/bootExamples/bean/User.java
+++ src/main/java/com/reke/learn/bootExamples/bean/User.java
@@ -1,14 +1,66 @@
 package com.reke.learn.bootExamples.bean;
 
 
-
+import java.util.Date;
 
 public class User {
 
 
 	private long id;
 	private String name;
-
+	private String password;
+	private String nickName;
+	private int gender;
+	private String mobile;
+	private String mobileArea;
+	private String email;
+	private String logo;
+	private String address;
+	private String registIp;
+	private String lastIp;
+	private int loginCount;
+	private String sign;
+	private double remain;
+	private double exchangeNum;
+	private int bingoNum;
+	private int regType;
+	private int type;
+	private int status;
+	private Date regTime;
+	private Date lastLoginTime;
+	private int chargeNum;
+	private Date lastChargeTime;
+	private float point;
+	private float currentMonthPoint;
+	private float lastMonthPoint;
+	private int level;
+	private double chargeTotalMoney;
+	private int buyTotalCoin;
+	private int takePartNum;
+	private int buyNum;
+	private int hasPayPass;
+	private int createNum;
+	private String did;
+	private String idfaImei;
+	private String mac;
+	private String net;
+	private int os;
+	private String osv;
+	private String ver;
+	private String r;
+	private String brand;
+	private int channelId;
+	private String umengToken;
+	private String jpushToken;
+	private String getuiToken;
+	private int noticeSwitch;
+	private String idCard;
+	private double totalCommission;
+	private double totalReward;
+	private int perBuyCoin;
+	private int showRewardCartoon;
+	private long shareUserId;
+	private int shareNum;
 
 	public User(){}
 
@@ -30,6 +82,428 @@ public class User {
 		this.name = name;
 	}
 
+	public String getPassword() {
+		return password;
+	}
 
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public int getGender() {
+		return gender;
+	}
+
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getMobileArea() {
+		return mobileArea;
+	}
+
+	public void setMobileArea(String mobileArea) {
+		this.mobileArea = mobileArea;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLogo() {
+		return logo;
+	}
+
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getRegistIp() {
+		return registIp;
+	}
+
+	public void setRegistIp(String registIp) {
+		this.registIp = registIp;
+	}
+
+	public String getLastIp() {
+		return lastIp;
+	}
+
+	public void setLastIp(String lastIp) {
+		this.lastIp = lastIp;
+	}
+
+	public int getLoginCount() {
+		return loginCount;
+	}
+
+	public void setLoginCount(int loginCount) {
+		this.loginCount = loginCount;
+	}
+
+	public String getSign() {
+		return sign;
+	}
+
+	public void setSign(String sign) {
+		this.sign = sign;
+	}
+
+	public double getRemain() {
+		return remain;
+	}
+
+	public void setRemain(double remain) {
+		this.remain = remain;
+	}
+
+	public double getExchangeNum() {
+		return exchangeNum;
+	}
+
+	public void setExchangeNum(double exchangeNum) {
+		this.exchangeNum = exchangeNum;
+	}
+
+	public int getBingoNum() {
+		return bingoNum;
+	}
+
+	public void setBingoNum(int bingoNum) {
+		this.bingoNum = bingoNum;
+	}
+
+	public int getRegType() {
+		return regType;
+	}
+
+	public void setRegType(int regType) {
+		this.regType = regType;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public Date getRegTime() {
+		return regTime;
+	}
+
+	public void setRegTime(Date regTime) {
+		this.regTime = regTime;
+	}
+
+	public Date getLastLoginTime() {
+		return lastLoginTime;
+	}
+
+	public void setLastLoginTime(Date lastLoginTime) {
+		this.lastLoginTime = lastLoginTime;
+	}
+
+	public int getChargeNum() {
+		return chargeNum;
+	}
+
+	public void setChargeNum(int chargeNum) {
+		this.chargeNum = chargeNum;
+	}
+
+	public Date getLastChargeTime() {
+		return lastChargeTime;
+	}
+
+	public void setLastChargeTime(Date lastChargeTime) {
+		this.lastChargeTime = lastChargeTime;
+	}
+
+	public float getPoint() {
+		return point;
+	}
+
+	public void setPoint(float point) {
+		this.point = point;
+	}
+
+	public float getCurrentMonthPoint() {
+		return currentMonthPoint;
+	}
+
+	public void setCurrentMonthPoint(float currentMonthPoint) {
+		this.currentMonthPoint = currentMonthPoint;
+	}
+
+	public float getLastMonthPoint() {
+		return lastMonthPoint;
+	}
+
+	public void setLastMonthPoint(float lastMonthPoint) {
+		this.lastMonthPoint = lastMonthPoint;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public double getChargeTotalMoney() {
+		return chargeTotalMoney;
+	}
+
+	public void setChargeTotalMoney(double chargeTotalMoney) {
+		this.chargeTotalMoney = chargeTotalMoney;
+	}
+
+	public int getBuyTotalCoin() {
+		return buyTotalCoin;
+	}
+
+	public void setBuyTotalCoin(int buyTotalCoin) {
+		this.buyTotalCoin = buyTotalCoin;
+	}
+
+	public int getTakePartNum() {
+		return takePartNum;
+	}
+
+	public void setTakePartNum(int takePartNum) {
+		this.takePartNum = takePartNum;
+	}
+
+	public int getBuyNum() {
+		return buyNum;
+	}
+
+	public void setBuyNum(int buyNum) {
+		this.buyNum = buyNum;
+	}
+
+	public int getHasPayPass() {
+		return hasPayPass;
+	}
+
+	public void setHasPayPass(int hasPayPass) {
+		this.hasPayPass = hasPayPass;
+	}
+
+	public int getCreateNum() {
+		return createNum;
+	}
+
+	public void setCreateNum(int createNum) {
+		this.createNum = createNum;
+	}
+
+	public String getDid() {
+		return did;
+	}
+
+	public void setDid(String did) {
+		this.did = did;
+	}
+
+	public String getIdfaImei() {
+		return idfaImei;
+	}
+
+	public void setIdfaImei(String idfaImei) {
+		this.idfaImei = idfaImei;
+	}
+
+	public String getMac() {
+		return mac;
+	}
+
+	public void setMac(String mac) {
+		this.mac = mac;
+	}
+
+	public String getNet() {
+		return net;
+	}
+
+	public void setNet(String net) {
+		this.net = net;
+	}
+
+	public int getOs() {
+		return os;
+	}
+
+	public void setOs(int os) {
+		this.os = os;
+	}
+
+	public String getOsv() {
+		return osv;
+	}
+
+	public void setOsv(String osv) {
+		this.osv = osv;
+	}
+
+	public String getVer() {
+		return ver;
+	}
+
+	public void setVer(String ver) {
+		this.ver = ver;
+	}
+
+	public String getR() {
+		return r;
+	}
+
+	public void setR(String r) {
+		this.r = r;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getChannelId() {
+		return channelId;
+	}
+
+	public void setChannelId(int channelId) {
+		this.channelId = channelId;
+	}
+
+	public String getUmengToken() {
+		return umengToken;
+	}
+
+	public void setUmengToken(String umengToken) {
+		this.umengToken = umengToken;
+	}
+
+	public String getJpushToken() {
+		return jpushToken;
+	}
+
+	public void setJpushToken(String jpushToken) {
+		this.jpushToken = jpushToken;
+	}
+
+	public String getGetuiToken() {
+		return getuiToken;
+	}
+
+	public void setGetuiToken(String getuiToken) {
+		this.getuiToken = getuiToken;
+	}
+
+	public int getNoticeSwitch() {
+		return noticeSwitch;
+	}
+
+	public void setNoticeSwitch(int noticeSwitch) {
+		this.noticeSwitch = noticeSwitch;
+	}
+
+	public String getIdCard() {
+		return idCard;
+	}
+
+	public void setIdCard(String idCard) {
+		this.idCard = idCard;
+	}
+
+	public double getTotalCommission() {
+		return totalCommission;
+	}
+
+	public void setTotalCommission(double totalCommission) {
+		this.totalCommission = totalCommission;
+	}
+
+	public double getTotalReward() {
+		return totalReward;
+	}
+
+	public void setTotalReward(double totalReward) {
+		this.totalReward = totalReward;
+	}
+
+	public int getPerBuyCoin() {
+		return perBuyCoin;
+	}
+
+	public void setPerBuyCoin(int perBuyCoin) {
+		this.perBuyCoin = perBuyCoin;
+	}
+
+	public int getShowRewardCartoon() {
+		return showRewardCartoon;
+	}
+
+	public void setShowRewardCartoon(int showRewardCartoon) {
+		this.showRewardCartoon = showRewardCartoon;
+	}
+
+	public long getShareUserId() {
+		return shareUserId;
+	}
+
+	public void setShareUserId(long shareUserId) {
+		this.shareUserId = shareUserId;
+	}
+
+	public int getShareNum() {
+		return shareNum;
+	}
+
+	public void setShareNum(int shareNum) {
+		this.shareNum = shareNum;
+	}
 
 }
