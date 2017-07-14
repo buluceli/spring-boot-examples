2bec344013705887d1ba83dfc243c792862269a3
buluceli <xwlyj@126.com>
2017-07-14 15:50:35 +0800

    share  wechat

--- src/main/java/com/reke/learn/bootExamples/controller/UserController.java
+++ src/main/java/com/reke/learn/bootExamples/controller/UserController.java
@@ -3,6 +3,7 @@ package com.reke.learn.bootExamples.controller;
 
 import com.reke.learn.bootExamples.bean.User;
 import com.reke.learn.bootExamples.dao.UserMapper;
+import com.reke.learn.bootExamples.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 	@Autowired
-	private UserMapper userMapper ;
+	private UserService userService ;
 
 	@RequestMapping("/user/{userId}")
 	public User get(@PathVariable long userId){
-		User user = userMapper.selectByPrimaryKey(userId);
+		User user = userService.selectByPrimaryKey(userId);
 		return user ;
 	}
 }
--- src/main/java/com/reke/learn/bootExamples/service/user/impl/UserServiceImpl.java
+++ src/main/java/com/reke/learn/bootExamples/service/user/impl/UserServiceImpl.java
@@ -7,13 +7,13 @@ import com.reke.learn.bootExamples.service.base.impl.BaseServiceImpl;
 import com.reke.learn.bootExamples.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
 
 
-@Component
-@Transactional
+@Service
 public class UserServiceImpl extends BaseServiceImpl<User, Long> implements UserService {
 
 	@Autowired
